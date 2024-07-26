66685e7eb088298ffce5dd63f911b04c25f3fd66
Getaneh48 <getaneh11@gmail.com>
2024-07-26 04:30:55 +0000

    Implement a function named initializeRooms and it return an array of 3 ClassRoom objects with the sizes 19, 20, and 34 (in this order)

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from './0-classroom.js';
+export default function initializeRooms() {
+  const crooms = [];
+  crooms.push(new ClassRoom(19));
+  crooms.push(new ClassRoom(20));
+  crooms.push(new ClassRoom(34));
+
+  return crooms;
+}
